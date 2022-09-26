a187584792dcd034394ccf97c1a4b3bc47bfd61d
Agusti B.R <agustibr@users.noreply.github.com>
2022-09-26 15:21:20 +0200

    feat(bridge): grant access to private spaces once verified by access code (#6)
    * feat(bridge): grant access to private spaces once verified by access code
    
    * Suggestions applied
    
    Co-authored-by: Francisco Bolívar <francisco.bolivar@nazaries.com>

---
+++ spec/lib/overrides_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# We make sure that the checksum of the file overriden is the same
+# as the expected. If this test fails, it means that the overriden
+# file should be updated to match any change/bug fix introduced in the core
+checksums = [
+  {
+    package: "decidim-access_codes",
+    files: {
+      "/app/controllers/decidim/access_codes/verification/authorizations_controller.rb" => "b9284d731ff0ed3c729c9746120b5373"
+    }
+  }
+]
+
+describe "Overriden files", type: :view do
+  # rubocop:disable Rails/DynamicFindBy
+  checksums.each do |item|
+    spec = ::Gem::Specification.find_by_name(item[:package])
+
+    item[:files].each do |file, signature|
+      it "#{spec.gem_dir}#{file} matches checksum" do
+        expect(md5("#{spec.gem_dir}#{file}")).to eq(signature)
+      end
+    end
+  end
+  # rubocop:enable Rails/DynamicFindBy
+
+  private
+
+  def md5(file)
+    Digest::MD5.hexdigest(File.read(file))
+  end
+end
